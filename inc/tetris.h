4cd6dbe3a9a6154c551d859ec2ffe3adc9e1d9b4
yliu <yliu@XLIJFELKNIX.local>
2024-01-05 11:40:24 +0900

    [feat] comment out macro OBAY_ORIGINAL_CODE to follow the pdf instruction

--- inc/tetris.h
+++ inc/tetris.h
@@ -18,8 +18,8 @@
 #define STARTING_TIME 400000
 #define INTERVAL_MICROSECONDS 1000000
 
-// comment out this line to obay to cursus pdf
-#define OBAY_ORIGINAL_CODE
+// comment out this line to obay cursus subject pdf requirement
+// #define OBAY_ORIGINAL_CODE
 
 // macro for readability
 #define NUMBER_OF_TOTAL_SHAPES (sizeof(g_StructsArray) / sizeof(g_StructsArray[0]))
